73ca5f3d73f648a697ceb91c9578cc02fe9b73ba
Elecshen <elecshen@yandex.com>
2026-07-27 18:31:41 +0700

    api env integration

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://api.dev.192-144-12-78.nip.io";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://api.dev.192-144-12-78.nip.io";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
